4d974a65afd1ced6e9591c722a88426cdb2bed57
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-07-01 11:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask sample application on appservcie"
+    return "Flask sample application on appservcie updated"
 
 
 if __name__ == "__main__":
